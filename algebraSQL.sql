349986d0c83e17d33c337023e4a75a9083cd7aed
Ian <ianhdez2020@gmail.com>
2025-03-05 00:29:58 -0600

    Ejercicio: Traducción de álgebra relacional a SQL Terminado

--- algebraSQL.sql
+++ algebraSQL.sql
@@ -62,6 +62,17 @@ JOIN Elenco E Pe.titulo = E.titulo and Pe.año = E.año
 JOIN Actor A ON e.nombre = a.nombre
 WHERE A.nombre ='Tom Cruise'
 
+-- 4) Dirección de los estudios en los que se han filmado películas con más de tres horas de duración en las que han actuado Salma Hayek o Antonio Banderas.
+-- algebra: PI_Direccion(estudio><(SL_Duracion>3h Pelicula><(SL_nombre= Salma Hayek or nombre=Antonio Banderas Elenco)))
+-- sql: 
+SELECT Direccion 
+FROM Estudio E
+JOIN Pelicula Pe ON Pe.nomestudio = E.nomestudio
+JOIN Elenco E ON  Pe.titulo = E.titulo and Pe.año = E.año
+WHERE Pe.duracion > 3 
+AND ( E.nombre = 'Antonio Banderas' OR E.nombre = 'Salma Hayek' )
+
+
 -- 5) Elenco de la película "Romeo y Julieta" de la producción del año 1938.
 -- algebra: PI_nombre(SL_Año= 1938 and titulo="Romeo y Julieta" Elenco)
 -- SQL:
@@ -70,8 +81,55 @@ FROM Elenco
 WHERE año= 1938 AND titulo="Romeo y Julieta";
 
 -- 6)  Nombre y teléfono de los actores que han aparecido en películas en las que el  productor ha sido George Lucas.
+-- algebra: PI_Nombre, Telefono Actor><(elenco><(pelicula><(SL_nombre='George Lucas' Productor)))
+-- SQL:
+SELECT A.Nombre AND A.Telefono
+FROM Actor
+JOIN Elenco E ON A.nombre = E.nombre
+JOIN Pelicula Pe ON E.titulo = Pe.titulo AND E.año = Pe.año
+JOIN Productor P ON Pe.idProductor = P.idProductor
+WHERE P.nombre='George Lucas'
 
 -- 7) Nombres de los actores que han participado en películas filmadas entre 1995 y el 2000.
 -- algebra: PI_Nombre(SL_año >= 1995 AND año<= 2000 Elenco)
 -- SQL:
 SELECT nombre FROM Elenco WHERE año BETWEEN 1995 AND 2000;
+
+-- 8)Nombre de los productores que han filmado películas para la "Universal Pictures".
+-- algebra: PI_Nombre Productor ><(Pelicula><(SL_nomestudio='Universal Pictures'))
+-- SQL:
+SELECT P.nombre
+FROM Productor P
+JOIN Pelicula Pe ON P.idProductor = Pe.idProductor
+JOIN Estudio E ON E.nomestudio = P.nomestudio
+WHERE E.nomestudio='Universal Pictures'
+
+-- 9) Nombre de los actores con más 60 años de Edad que participaron en la película del "Mago de OZ".
+-- algebra: PI_nombre Actor(SL_fecha> 1965 Actor><(Elenco ><(SL_titulo='Mago de OZ' Pelicula)))
+-- SQL:
+SELECT A.nombre
+FROM Actor A
+JOIN Elenco E ON A.nombre = E.nombre
+JOIN Pelicula ON Elenco.titulo = Pelicula.titulo AND Elenco.año = Pelicula.año
+WHERE E.titulo = 'Mago de OZ' AND A.fecha >1965
+
+-- 10) Nombre de los productores que han trabajado tanto para los estudios "FOX" como para "MGM".
+-- Algebra: R1 =  PI_Nombre Productor><(Pelicula><(SL_nomestudio = 'FOX' Estudio)))
+-- R2 = PI_Nombre Productor><(Pelicula><(SL_nomestudio = 'MGM' Estudio)))
+-- R1 U R2
+-- SQL:
+SELECT DISTINCT P.nombre 
+FROM Productor P
+JOIN Pelicula Pe ON P.idproductor = Pe.idProductor
+JOIN Estudio E ON Pe.nomestudio = E.nomestudio
+WHERE E.nomestudio = 'FOX'
+
+INTERSECT
+
+SELECT DISTINCT P.nombre 
+FROM Productor P
+JOIN Pelicula Pe ON P.idproductor = Pe.idProductor
+JOIN Estudio E ON Pe.nomestudio = E.nomestudio
+WHERE E.nomestudio = 'MGM'
+
+
